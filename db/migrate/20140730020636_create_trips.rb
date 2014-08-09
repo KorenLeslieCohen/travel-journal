29c0aab444ffeb473546fdf8979a64a2cb53d1a0
Koren Leslie Cohen <koren.cohen@gmail.com>
2014-08-09 05:26:26 -0400

    added seed data, fixed form checkboxes, worked on styling, etc.

--- db/migrate/20140730020636_create_trips.rb
+++ db/migrate/20140730020636_create_trips.rb
@@ -3,10 +3,14 @@ class CreateTrips < ActiveRecord::Migration
     create_table :trips do |t|
       t.string :trip_name
       t.string :location
+      t.string :date
       t.integer :user_id
       t.text :to_pack
       t.text :to_do
       t.integer :budget
+      t.text :things_i_did
+      t.text :things_i_liked
+      t.text :things_i_didnt_like
 
       t.timestamps
     end
